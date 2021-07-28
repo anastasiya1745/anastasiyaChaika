1788c04690abf4bde5a0bfc628af5b5b362c1182
anastasiya1745 <anastasiya.od@gmail.com>
2021-07-28 17:45:17 +0300

    update EngCalculator methods

--- HW_folder/src/com/hillel/lesson7/Main.java
+++ HW_folder/src/com/hillel/lesson7/Main.java
@@ -6,7 +6,6 @@ public class Main {
         SimpleCalculator simpleCalculator = new SimpleCalculator();
         EngCalculator engCalculator = new EngCalculator();
         FinancialCalculator financialCalculator = new FinancialCalculator();
-
         double result1 =  simpleCalculator.Divide(4,1);
         double result2 = engCalculator.sinValue(engCalculator.moveDegreetoRadian(2));
         double result3 = financialCalculator.uahMoveToDoll(2400, 0.037);
